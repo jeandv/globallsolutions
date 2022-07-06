e175ce2f6ed3f4d5ba46c3bca9e08f70e828242d
Jean Rondon <jeanrdev@gmail.com>
2022-07-05 22:49:21 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,121 @@
+(function ($) {
+    "use strict";
+
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({ filter: $(this).data('filter') });
+    });
+
+
+    // Team carousel
+    $(".team-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav: true,
+        navText: [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: true,
+        loop: true,
+        center: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
+
+})(jQuery);
+
+function _class(name) {
+    return document.getElementsByClassName(name);
+}
+
+let tabPanes = _class("tab-header")[0].getElementsByTagName("div");
+
+for (let i = 0; i < tabPanes.length; i++) {
+    tabPanes[i].addEventListener("click", function () {
+        _class("tab-header")[0].getElementsByClassName("active")[0].classList.remove("active");
+        tabPanes[i].classList.add("active");
+
+        _class("tab-indicator")[0].style.top = `calc(80px + ${i * 50}px)`;
+
+        _class("tab-content")[0].getElementsByClassName("active")[0].classList.remove("active");
+        _class("tab-content")[0].getElementsByTagName("div")[i].classList.add("active");
+
+    });
+}
---
+++ mail/contact.php
@@ -0,0 +1,20 @@
+<?php
+if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$to = "contacto@globallsolutions.cl";
+$subject = "$m_subject:  $name";
+$body = "Has recibido un nuevo mensaje desde el formulario de tu página web\n\n"."Aqui los detalles:\n\nNombre: $name\n\n\nEmail: $email\n\nTelefono: $m_subject\n\nMensaje: $message";
+$header = "From: $email";
+$header .= "Reply-To: $email";	
+
+if(!mail($to, $subject, $body, $header))
+  http_response_code(500);
+?>
